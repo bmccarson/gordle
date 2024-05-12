394f970e440e394c4f4b79eaa45b3bfb4ce01061
bmccarson <brian@mccarson.us>
2024-05-11 20:50:59 -0400

    create intl Play function to run a Game.

--- gordle/game.go
+++ gordle/game.go
@@ -1,5 +1,7 @@
 package gordle
 
+import "fmt"
+
 // Game holds all the information we need to play a game of gordle.
 type Game struct{}
 
@@ -8,4 +10,11 @@ func New() *Game {
 	g := &Game{}
 
 	return g
+}
+
+// Play runs the game.
+func (g *Game) Play() {
+	fmt.Println("Welcome to Gordle!")
+
+	fmt.Printf("Enter a guess:\n")
 }
