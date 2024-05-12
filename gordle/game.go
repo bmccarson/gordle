00af9c0f6042395be0ce8f4acac0631f32199f0d
bmccarson <brian@mccarson.us>
2024-05-11 20:47:47 -0400

    create intl New() func to intialize a Game.

--- gordle/game.go
+++ gordle/game.go
@@ -2,3 +2,10 @@ package gordle
 
 // Game holds all the information we need to play a game of gordle.
 type Game struct{}
+
+// New returns a Game, which can be used to Play.
+func New() *Game {
+	g := &Game{}
+
+	return g
+}
