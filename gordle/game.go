8962318742e64c43afabccffe7891dd8ea9addaa
bmccarson <brian@mccarson.us>
2024-05-16 18:02:31 -0400

    create ask function on Game that takes in and checks player input.

--- gordle/game.go
+++ gordle/game.go
@@ -4,7 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"os"
 )
+
+const solutionLength = 5
 
 // Game holds all the information we need to play a game of gordle.
 type Game struct {
@@ -25,4 +28,25 @@ func (g *Game) Play() {
 	fmt.Println("Welcome to Gordle!")
 
 	fmt.Printf("Enter a guess:\n")
+}
+
+// ask reads input unitl a valid suggestion is made and returned.
+func (g *Game) ask() []rune {
+	fmt.Printf("Enter a %d-character guess:\n", solutionLength)
+
+	for {
+		playerInput, _, err := g.reader.ReadLine()
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "Gordle failed to read your guess: %s\n", err.Error())
+			continue
+		}
+
+		guess := []rune(string(playerInput))
+
+		if len(guess) != solutionLength {
+			_, _ = fmt.Fprintf(os.Stderr, "Your attempt is invalid with Gordle's solution! Expected %d characters, got %d.\n", solutionLength, len(guess))
+		} else {
+			return guess
+		}
+	}
 }
