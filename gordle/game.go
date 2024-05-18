e2186dde6c053a4c173b2cde60f8473817a37237
bmccarson <brian@mccarson.us>
2024-05-18 15:33:53 -0400

    create method to validate guess and impl in ask

--- gordle/game.go
+++ gordle/game.go
@@ -45,10 +45,23 @@ func (g *Game) ask() []rune {
 
 		guess := []rune(string(playerInput))
 
-		if len(guess) != solutionLength {
-			_, _ = fmt.Fprintf(os.Stderr, "Your attempt is invalid with Gordle's solution! Expected %d characters, got %d.\n", solutionLength, len(guess))
+		err = g.validateGuess(guess)
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "Your attempt is invalid with Gordle's solution: %s.\n", err.Error())
 		} else {
 			return guess
 		}
 	}
+}
+
+// errInvalidWordLength is returned when the guess has the wrong number of characters.
+var errInvalidWordLength = fmt.Errorf("invalid guess, word doesn't have teh same number of characters as the solution")
+
+// validateGuess ensures the guess is valid enough.
+func (g *Game) validateGuess(guess []rune) error {
+	if len(guess) != solutionLength {
+		return fmt.Errorf("expected %d, got %d, %w", solutionLength, len(guess), errInvalidWordLength)
+	}
+
+	return nil
 }
