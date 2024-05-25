73bb8ec29d6294e4bc3958a049ad5e44bfbdba8d
bmccarson <brian@mccarson.us>
2024-05-25 15:27:35 -0400

    update main to impl updated new signature

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"os"
 
 	"github.com/bmccarson/gordle/gordle"
@@ -9,9 +11,17 @@ import (
 const maxAttempts = 6
 
 func main() {
-	solution := "hello"
+	corpus, err := gordle.ReadCorpus("corpus/english.txt")
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "unable to read corpus: %s", err)
+		return
+	}
 
-	g := gordle.New(os.Stdin, solution, maxAttempts)
+	g, err := gordle.New(bufio.NewReader(os.Stdin), corpus, maxAttempts)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "unable to start game: %s", err)
+		return
+	}
 
 	g.Play()
 }
